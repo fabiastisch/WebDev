8b97140d79c81bb53cee9bbfc6ed36fc1747c544
Fabian Weber <faweber@stud.hs-heilbronn.de>
2021-07-15 03:23:45 +0200

    add some stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {FirstComponent} from "./first/first.component";
+import {SecondComponent} from "./second/second.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'first', component: FirstComponent},
+  {path: 'second', component: SecondComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatSliderModule} from "@angular/material/slider";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import {HeaderComponent} from "./header/header.component";
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import {MatRadioModule} from "@angular/material/radio";
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FirstComponent,
+    SecondComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
